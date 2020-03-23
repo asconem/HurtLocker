a75ef6b9c69e90f9157e2c13c1ffea9f05f7ec33
asconem <asconem@gmail.com>
2020-03-23 14:35:49 -0400

    Finally cracked a few more regex matches

--- src/main/java/ItemParser.java
+++ src/main/java/ItemParser.java
@@ -15,7 +15,7 @@ public class ItemParser {
         return line.split("[^a-zA-Z0-9/.]");
     }
 
-    public static String matchName(String name) {
+    public static String nameMatch(String name) {
         Pattern apples = Pattern.compile("[aA][pP][pP][lL][eE][sS]");
         Pattern bread = Pattern.compile("[bB][rR][eE][aA][dD]");
         Pattern cookies = Pattern.compile("[cC][oO0][oO0][kK][iI][eE][sS]");
@@ -37,5 +37,26 @@ public class ItemParser {
             throw new IllegalArgumentException();
         }
         return name;
+    }
+
+    public static void priceMatch(String price) {
+        Pattern pattern = Pattern.compile("([0-9]+\\.[0-9]+)");
+        if(!pattern.matcher(price).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void typeMatch(String type) {
+        Pattern pattern = Pattern.compile("Food");
+        if(!pattern.matcher(type).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void expirationMatch(String expiration) {
+        Pattern pattern = Pattern.compile("([0-9]+\\/[0-9]+\\/[0-9]+)");
+        if(!pattern.matcher(expiration).matches()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
